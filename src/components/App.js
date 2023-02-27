e8168886b741b3c6bcdf7b128a38768a0fc6f65e
Aiden Zafar <+>
2023-02-26 19:43:00 -0500

    tasks and tasklist

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,20 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+
+  const [task, setTasks] = useState(TASKS)
+  const [selectedCategory, setSelectedCategory] = useState(task)
+
+  const handleDelete = (deleteTaskText) => {
+    setTasks(TASKS.filter(task => task.text!== deleteTaskText))
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      <TaskList TASKS={TASKS} handleDelete={handleDelete}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({TASKS, handleDelete}) {
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {TASKS.map((task) => (
+        <Task key={task.text} text={task.test} category={task.category} handleDelete={handleDelete}/>
+      ))}
     </div>
   );
 }
